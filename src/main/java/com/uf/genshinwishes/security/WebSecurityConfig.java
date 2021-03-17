2b2a4df0dbd650dd184639cf2baa5cf5d5e5e1d8
Ishou <Ishou@users.noreply.github.com>
2021-03-17 13:30:08 +0100

    chore: add monitoring

--- src/main/java/com/uf/genshinwishes/security/WebSecurityConfig.java
+++ src/main/java/com/uf/genshinwishes/security/WebSecurityConfig.java
@@ -13,9 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -24,10 +22,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -51,24 +46,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .and()
             .authorizeRequests()
-            .antMatchers("/oauth2/**", "/login/**", "/user", "/actuator/info").permitAll()
+            .antMatchers("/oauth2/**", "/login/**", "/user", "/actuator/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .oauth2Login()
-            .successHandler(new AuthenticationSuccessHandler() {
-                @Override
-                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                    String email = (String) ((DefaultOAuth2User) authentication.getPrincipal()).getAttributes().get("email");
-                    User user = userService.findByEmail(email);
+            .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                String email = (String) ((DefaultOAuth2User) authentication.getPrincipal()).getAttributes().get("email");
+                User user = userService.findByEmail(email);
 
-                    if (user == null) {
-                        user = userService.insertUser(email);
-                    } else {
-                        userService.updateLastLoggingDate(user);
-                    }
-
-                    httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(user));
+                if (user == null) {
+                    user = userService.insertUser(email);
+                } else {
+                    userService.updateLastLoggingDate(user);
                 }
+
+                httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(user));
             })
             .and()
             .exceptionHandling()
