be0cd9b54ee17988acdff063cb61afb92c29f377
Ishou <Ishou@users.noreply.github.com>
2021-09-01 11:33:50 +0200

    feat: temporarily remove global stats

--- src/main/java/com/uf/genshinwishes/config/CachingConfig.java
+++ src/main/java/com/uf/genshinwishes/config/CachingConfig.java
@@ -1,16 +1,9 @@
 package com.uf.genshinwishes.config;
 
-import com.uf.genshinwishes.dto.BannerDTO;
-import com.uf.genshinwishes.model.BannerType;
-import com.uf.genshinwishes.model.User;
-import com.uf.genshinwishes.service.BannerService;
-import com.uf.genshinwishes.service.PublicStatsService;
 import com.uf.genshinwishes.service.UserService;
 import com.uf.genshinwishes.service.WishService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -18,48 +11,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Arrays;
-import java.util.List;
-
 @EnableCaching
 @EnableScheduling
 @Configuration
 public class CachingConfig {
 
     @Autowired
-    private PublicStatsService publicStatsService;
-    @Autowired
-    private BannerService bannerService;
-    @Autowired
     private WishService wishService;
     @Autowired
     private UserService userService;
 
-    @Getter
-    private boolean loaded = false;
-
     @Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("publicStats", "usersCount", "wishesCount");
-    }
-
-    @Scheduled(fixedDelay = 12000000, initialDelay = 0)
-    public void publicStatsUpdate() {
-        publicStatsService.updateStatsFor(BannerType.ALL, null);
-        publicStatsService.updateStatsFor(BannerType.CHARACTER_EVENT, null);
-        publicStatsService.updateStatsFor(BannerType.WEAPON_EVENT, null);
-
-        List<BannerDTO> banners = bannerService.findAll();
-
-        banners.forEach(b -> {
-            publicStatsService.updateStatsFor(
-                b.getGachaType(),
-                b.getGachaType() == BannerType.CHARACTER_EVENT || b.getGachaType() == BannerType.WEAPON_EVENT
-                    ? b.getId()
-                    : null);
-        });
-
-        loaded = true;
     }
 
     @Scheduled(fixedDelay = 300000, initialDelay = 0)
